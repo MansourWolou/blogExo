79bf833158b457a91b8be1ad56d9cbb526df9f2d
MansourWolou <Wolou0444@gmail.com>
2022-10-06 11:55:19 +0200

    project init

---
+++ blog/pages/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-container>
+    <p>coucou coco</p>
+
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "IndexPage",
+  data () {
+    return {
+      autocomplete: '',
+      search: '',
+      list: null
+    }
+  },
+
+}
+
+</script>
---
+++ expressApp/README.md
@@ -0,0 +1,8 @@
+Api rest qui récupère / ajoutes des data dans la bdd local et dessert la vue
+
+# bdd
+
+
+
+# structure
+
